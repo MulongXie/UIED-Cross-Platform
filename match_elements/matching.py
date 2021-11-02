2407b4f7dbf1e4132642e1295f5b88075d1bc0c1
MulongXie <dsh15325@163.com>
2021-11-02 15:58:50 +1100

    add ssim

--- match_elements/matching.py
+++ match_elements/matching.py
@@ -1,10 +1,14 @@
 import cv2
 from difflib import SequenceMatcher
+from skimage.measure import compare_ssim
 
 
 def dhash(image):
     image = cv2.resize(image, (9, 8), interpolation=cv2.INTER_CUBIC)
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    if image.ndim == 3:
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    else:
+        gray = image
     dhash_str = ''
     for i in range(8):
         for j in range(8):
@@ -25,12 +29,25 @@ def compare_hash(hash1, hash2):
     return 1 - n / len(hash1)
 
 
-def image_similarity(img1, img2, method='dhash'):
+def image_similarity(img1, img2, method='dhash', is_gray=False):
     similarity = -1  # from 0 to 1
     if method == 'dhash':
         h1 = dhash(img1)
         h2 = dhash(img2)
         similarity = compare_hash(h1, h2)
+    elif method == 'ssim':
+        multi_channel = True
+        if is_gray:
+            img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+            img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+            multi_channel = False
+        if img1.shape != img2.shape:
+            side = max(img1.shape[:2] + img2.shape[:2])
+            img1_cp = cv2.resize(img1, (side, side))
+            img2_cp = cv2.resize(img2, (side, side))
+            similarity = compare_ssim(img1_cp, img2_cp, multichannel=multi_channel)
+        else:
+            similarity = compare_ssim(img1, img2, multichannel=multi_channel)
     return similarity
 
 
