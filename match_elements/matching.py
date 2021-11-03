fd4a31ad34d44e0b905068ed43a03b3ecc0a285d
MulongXie <dsh15325@163.com>
2021-11-03 12:53:46 +1100

    add sift and surf for image similarity calculation

--- match_elements/matching.py
+++ match_elements/matching.py
@@ -29,7 +29,41 @@ def compare_hash(hash1, hash2):
     return 1 - n / len(hash1)
 
 
-def image_similarity(img1, img2, method='dhash', is_gray=False):
+def compare_sift_or_surf(img1, img2, method, ratio=1.5, draw_match=False):
+    if method == 'sift':
+        sift = cv2.xfeatures2d.SIFT_create()
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
+    elif method == 'surf':
+        # Initiate SURF detector
+        surf = cv2.xfeatures2d.SURF_create()
+        # find the keypoints and descriptors with SURF
+        kp1, des1 = surf.detectAndCompute(img1, None)
+        kp2, des2 = surf.detectAndCompute(img2, None)
+    else:
+        print('set method as either sift or surf')
+        return
+
+    # BFMatcher with default params
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+    # If there's a big difference between the best and second-best matches, this to be a quality match.
+    valid_matches = []
+    for best, second in matches:
+        if second.distance / best.distance > ratio:
+            valid_matches.append(best)
+
+    if draw_match:
+        board = cv2.drawMatches(img1, kp1, img2, kp2, valid_matches, None)
+        cv2.imshow('sift match', cv2.resize(board, (int(board.shape[1] * (200 / board.shape[0])), 200)))
+        cv2.waitKey()
+        cv2.destroyWindow('sift match')
+    return len(valid_matches) / len(kp2)
+
+
+def image_similarity(img1, img2, method='dhash', is_gray=False,
+                     draw_match=False, match_distance_ratio=1.5):
     similarity = -1  # from 0 to 1
     if method == 'dhash':
         h1 = dhash(img1)
@@ -48,6 +82,10 @@ def image_similarity(img1, img2, method='dhash', is_gray=False):
             similarity = compare_ssim(img1_cp, img2_cp, multichannel=multi_channel)
         else:
             similarity = compare_ssim(img1, img2, multichannel=multi_channel)
+    elif method == 'sift':
+        similarity = compare_sift_or_surf(img1, img2, 'sift', match_distance_ratio, draw_match=draw_match)
+    elif method == 'surf':
+        similarity = compare_sift_or_surf(img1, img2, 'surf', match_distance_ratio, draw_match=draw_match)
     return similarity
 
 
